edcc7c484fc1bf43935b2c600732d0b24f9adaa7
XiangyuLiualex <63183143+XiangyuLiualex@users.noreply.github.com>
2023-03-05 15:01:55 -0800

    Add files via upload

---
+++ BackEnd/bin/dbServer.js
@@ -0,0 +1,173 @@
+var sqlite3 = require("sqlite3");
+var express= require("express");
+
+var app=express();
+app.use(express.json());
+var dbPath = '../db/master.db'
+
+// search all table
+function getAllInfo(tableName){
+    var db = new sqlite3.Database(dbPath,(err,data)=>{
+      let ans={stat:"",content:""}
+        if(!err){
+            db.all('SELECT * FROM "' + tableName + '"',(err, data)=>{
+                if(!err){
+                    console.log(typeof data)
+                    console.log(data)
+                      ans['stat']=1;
+                      ans['content']=data;
+                      return JSON.stringify(ans)
+                }
+                else{
+                    ans['stat']=0;
+                    ans['content']="sorry did not find  '" + tableName + "' info";
+                    console.log(err.message)
+                    return JSON.stringify(ans)
+                }
+            })
+        }
+    })
+  }
+
+
+  // search item by id
+function getInfoByID(tableName,IdName,Id){
+    var db = new sqlite3.Database(dbPath,(err,data)=>{
+      let ans={stat:"",content:""}
+        if(!err){
+            db.all('SELECT * FROM "' + tableName + '" where "' + IdName + '"="' + Id + '"',(err, data)=>{
+                if(!err){
+                    console.log(typeof data)
+                    console.log(data)
+                      ans['stat']=1;
+                      ans['content']=data;
+                      return JSON.stringify(ans)
+                }
+                else{
+                    ans['stat']=0;
+                    ans['content']="sorry did not find '"+tableName +"' info by '"+IdName+"'";
+                    console.log(err.message)
+                    return JSON.stringify(ans)
+                }
+            })
+        }
+    })
+  }
+
+
+
+
+// edit item quantity
+function editItemQuantity(tableName,IdName,Id,changeNum){
+//   console.log("chairId: "+chairId+"    changeNumber: "+changeNum)
+  let ans = { stat: "", content: "" }
+  //print bubby
+  console.log("decrease the number of "+Id+" by "+changeNum)
+  var db = new sqlite3.Database(dbPath, (err, data) => {
+      if (!err) {
+          // check if the username and password is correct
+          db.all('SELECT quantity FROM "'+tableName+'" where "'+IdName+'"="' + Id + '"', (err, data) => {
+            
+               console.log(data)
+              if (data.length == 1) {
+                  console.log(data[0]['quantity'])
+                  var quantity=data[0]['quantity']
+                  let sql;
+                  sql = 'UPDATE "'+tableName+'" SET quantity = ? WHERE "'+IdName+'" = ?';
+                  newQuantity=quantity-changeNum
+                  // newQuantity=98
+                  db.run(sql, [newQuantity, Id], (err) => {
+                      console.log("Reset quantity sucessfully")
+                      if (!err) {
+                          ans['stat'] = 1;
+                          ans['content'] = 'You have changed the "'+tableName+'" quantity successfully!';
+                          return JSON.stringify(ans)
+                      } else {
+                          ans['stat'] = 0;
+                          ans['content'] = 'You have entered the right "'+IdName+'", but changing failed!';
+                          return JSON.stringify(ans)
+                      }
+                  });
+              }
+              else {
+                  ans['stat'] = 0;
+                  ans['content'] = 'The "'+IdName+'" is not unique';
+                  return JSON.stringify(ans)
+              }
+          })
+      }
+  })
+}
+
+
+
+
+// create order table
+function insertOrder(insertOrderData){
+  var db = new sqlite3.Database(dbPath,(err,data)=>{
+    let ans={stat:"",content:""}
+      if(!err){
+        console.log("1111")
+        db.run('CREATE TABLE IF NOT EXISTS orderInfo(id  integer primary key autoincrement, orderId text, customerName text, customerAddress text, cardNumber text, exp_date text, secu_code integer, itemName text)',(err)=>{
+        console.log("2222")
+              if(!err){
+                      console.log('table is created sucessfully!')
+                    //   db.run('INSERT INTO orderInfo(orderId, customerName,customerAddress, cardNumber, exp_date, secu_code, itemName) values("AAAA","Alex","188 harvest rose","4563888855742057","05/16","826","Table1")',(err)=>{
+                        db.run('INSERT INTO orderInfo(orderId, customerName,customerAddress, cardNumber, exp_date, secu_code, itemName) values('+insertOrderData+')',(err)=>{
+                        if(!err){
+                            ans['stat']=1;
+                            ans['content']='Data add successfully!';
+                            return JSON.stringify(ans)
+                            }
+                        else{
+                            ans['stat']=0;
+                            ans['content']='Error!';
+                            console.log(err);
+                            return JSON.stringify(ans)
+                        }
+                    })
+              }
+              else{
+                console.log("yes, error!")
+                  console.log(err.message)
+              }
+          })
+      }
+  })
+}
+
+// add new version number
+function addVersion(versionNum){
+  var db = new sqlite3.Database(dbPath,(err,data)=>{
+    let ans={stat:"",content:""}
+      if(!err){
+          db.run('CREATE TABLE IF NOT EXISTS version(id  integer primary key autoincrement, versionNum integer)',(err)=>{
+              if(!err){
+                    //   console.log('count is created sucessfully!')
+                      db.run('INSERT INTO version(versionNum) values('+versionNum+')',(err)=>{
+                        if(!err){
+                            console.log("New version number added")
+                            ans['stat']=1;
+                            ans['content']='Data add successfully!';
+                            return JSON.stringify(ans)
+                            }
+                        else{
+                            ans['stat']=0;
+                            ans['content']='Error!';
+                            console.log(err);
+                            return JSON.stringify(ans)
+                        }
+                    })
+              }
+              else{
+                  console.log(err.message)
+              }
+          })
+      }
+  })
+}
+
+var iod='"ABDC","chao","188 harvest rose","4563888855742057","05/16","826","Table88"'
+addVersion(003)
+getInfoByID("version","versionNum",3)
+
