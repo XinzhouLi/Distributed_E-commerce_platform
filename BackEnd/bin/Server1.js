f307e09236daf5a260df6c167f861e5da3997aa1
Old Li's Mac <xinzhou.li9822@gmail.com>
2023-03-05 20:24:03 -0700

    Server1 import the DB operation

--- BackEnd/bin/Server1.js
+++ BackEnd/bin/Server1.js
@@ -1,5 +1,5 @@
 const DB = require('./dbServer')
-
+const {response} = require("express");
 const ioWithLoadBalancer = require('socket.io')(5100);
 ioWithLoadBalancer.on('connection', function (socket) {
     console.log('connected:', socket.client.id);
@@ -7,35 +7,60 @@ ioWithLoadBalancer.on('connection', function (socket) {
     socket.on('requestSingleItem', function(data) {
         // ***** Edit ******
         // connect to dbserver
-
+        let Jobj = JSON.parse(data)
+        let response = DB.getInfoByID(Jobj.tableName, Jobj.IdName, Jobj.Id)
         // ***** EDit ******
-        socket.emit('responseSingleItemInfo');
+        socket.emit('responseSingleItemInfo', response);
     });
 
     socket.on('requestAllCateInfo', function(data) {
         // ***** Edit ******
         // connect to dbserver
-
+        let Jobj = JSON.parse(data);
+        let response = DB.getAllInfo(Jobj.tableName);
         // ***** EDit ******
-        socket.emit('responseAllCateInfo');
+        socket.emit('responseAllCateInfo', response);
     });
 
     socket.on('addOrder', function(data) {
         // ***** Edit ******
         // connect to dbserver
-
+        let Jobj = JSON.parse(data);
+        let response = DB.insertOrder(Jobj.insertOrderData);
         // ***** EDit ******
         // only add to db, if sucess, emit 1
-        socket.emit('responseUserOrderStatus');
+        socket.emit('responseUserOrderStatus', response);
     });
 
     socket.on('requestOrderInfo', function(data) {
         // ***** Edit ******
         // connect to dbserver
-
+        let Jobj = JSON.parse(data);
+        let response = DB.getInfoByID(Jobj.tableName, Jobj.IdName, Jobj.Id)
         // ***** EDit ******
         // only add to db, if sucess, emit 1
-        socket.emit('responseOrderInfo');
+        socket.emit('responseOrderInfo', response);
     });
     
 });
+
+
+function copyDB(){
+    fs.copyFile('../db/master.db', './db/slave1.db', (err) => {
+        if (err) throw err;
+        console.log('master.db was copied to slave1.db');
+    });
+    fs.copyFile('./db/master.db', './db/slave2.db', (err) => {
+        if (err) throw err;
+        console.log('master.db was copied to slave2.db');
+    });
+    fs.copyFile('./db/master.db', './db/slave3.db', (err) => {
+        if (err) throw err;
+        console.log('master.db was copied to slave3.db');
+    });
+    fs.copyFile('./db/master.db', './db/slave4.db', (err) => {
+        if (err) throw err;
+        console.log('master.db was copied to slave4.db');
+    });
+}
+setInterval(copyDB, 3000);
--- BackEnd/bin/dbServer.js
+++ BackEnd/bin/dbServer.js
@@ -1,172 +1,166 @@
 var sqlite3 = require("sqlite3");
-var express= require("express");
+var express = require("express");
 
-var app=express();
+var app = express();
 app.use(express.json());
 var dbPath = '../db/master.db'
 
 // search all table
 // tableName example: chair 
-function getAllInfo(tableName){
-    var db = new sqlite3.Database(dbPath,(err,data)=>{
-      let ans={stat:"",content:""}
-        if(!err){
-            db.all('SELECT * FROM "' + tableName + '"',(err, data)=>{
-                if(!err){
+function getAllInfo(tableName) {
+    var db = new sqlite3.Database(dbPath, (err, data) => {
+        let ans = {stat: "", content: ""}
+        if (!err) {
+            db.all('SELECT * FROM "' + tableName + '"', (err, data) => {
+                if (!err) {
                     console.log(typeof data)
                     console.log(data)
-                      ans['stat']=1;
-                      ans['content']=data;
-                      return JSON.stringify(ans)
-                }
-                else{
-                    ans['stat']=0;
-                    ans['content']="sorry did not find  '" + tableName + "' info";
+                    ans['stat'] = 1;
+                    ans['content'] = data;
+                    return JSON.stringify(ans)
+                } else {
+                    ans['stat'] = 0;
+                    ans['content'] = "sorry did not find  '" + tableName + "' info";
                     console.log(err.message)
                     return JSON.stringify(ans)
                 }
             })
         }
     })
-  }
+}
 
 
-  // search item by id
-  //tableName example: bed
-  //IdName example: bedId
-  //Id example: b03
-function getInfoByID(tableName,IdName,Id){
-    var db = new sqlite3.Database(dbPath,(err,data)=>{
-      let ans={stat:"",content:""}
-        if(!err){
-            db.all('SELECT * FROM "' + tableName + '" where "' + IdName + '"="' + Id + '"',(err, data)=>{
-                if(!err){
+// search item by id
+//tableName example: bed
+//IdName example: bedId
+//Id example: b03
+function getInfoByID(tableName, IdName, Id) {
+    var db = new sqlite3.Database(dbPath, (err, data) => {
+        let ans = {stat: "", content: ""}
+        if (!err) {
+            db.all('SELECT * FROM "' + tableName + '" where "' + IdName + '"="' + Id + '"', (err, data) => {
+                if (!err) {
                     console.log(typeof data)
                     console.log(data)
-                      ans['stat']=1;
-                      ans['content']=data;
-                      return JSON.stringify(ans)
-                }
-                else{
-                    ans['stat']=0;
-                    ans['content']="sorry did not find '"+tableName +"' info by '"+IdName+"'";
+                    ans['stat'] = 1;
+                    ans['content'] = data;
+                    return JSON.stringify(ans)
+                } else {
+                    ans['stat'] = 0;
+                    ans['content'] = "sorry did not find '" + tableName + "' info by '" + IdName + "'";
                     console.log(err.message)
                     return JSON.stringify(ans)
                 }
             })
         }
     })
-  }
+}
 
 
-  // edit item quantity
-  //tableName example: bed
-  //IdName example: bedId
-  //Id example: b03
-  //changeNum: 1
-function editItemQuantity(tableName,IdName,Id,changeNum){
-  let ans = { stat: "", content: "" }
-  //print bubby
-  console.log("decrease the number of "+Id+" by "+changeNum)
-  var db = new sqlite3.Database(dbPath, (err, data) => {
-      if (!err) {
-          // check if the username and password is correct
-          db.all('SELECT quantity FROM "'+tableName+'" where "'+IdName+'"="' + Id + '"', (err, data) => {
-               console.log(data)
-              if (data.length == 1) {
-                  console.log(data[0]['quantity'])
-                  var quantity=data[0]['quantity']
-                  let sql;
-                  sql = 'UPDATE "'+tableName+'" SET quantity = ? WHERE "'+IdName+'" = ?';
-                  newQuantity=quantity-changeNum
-                  db.run(sql, [newQuantity, Id], (err) => {
-                      console.log("Reset quantity sucessfully")
-                      if (!err) {
-                          ans['stat'] = 1;
-                          ans['content'] = 'You have changed the "'+tableName+'" quantity successfully!';
-                          return JSON.stringify(ans)
-                      } else {
-                          ans['stat'] = 0;
-                          ans['content'] = 'You have entered the right "'+IdName+'", but changing failed!';
-                          return JSON.stringify(ans)
-                      }
-                  });
-              }
-              else {
-                  ans['stat'] = 0;
-                  ans['content'] = 'The "'+IdName+'" is not unique';
-                  return JSON.stringify(ans)
-              }
-          })
-      }
-  })
+// edit item quantity
+//tableName example: bed
+//IdName example: bedId
+//Id example: b03
+//changeNum: 1
+function editItemQuantity(tableName, IdName, Id, changeNum) {
+    let ans = {stat: "", content: ""}
+    //print bubby
+    console.log("decrease the number of " + Id + " by " + changeNum)
+    var db = new sqlite3.Database(dbPath, (err, data) => {
+        if (!err) {
+            // check if the username and password is correct
+            db.all('SELECT quantity FROM "' + tableName + '" where "' + IdName + '"="' + Id + '"', (err, data) => {
+                console.log(data)
+                if (data.length == 1) {
+                    console.log(data[0]['quantity'])
+                    var quantity = data[0]['quantity']
+                    let sql;
+                    sql = 'UPDATE "' + tableName + '" SET quantity = ? WHERE "' + IdName + '" = ?';
+                    newQuantity = quantity - changeNum
+                    db.run(sql, [newQuantity, Id], (err) => {
+                        console.log("Reset quantity sucessfully")
+                        if (!err) {
+                            ans['stat'] = 1;
+                            ans['content'] = 'You have changed the "' + tableName + '" quantity successfully!';
+                            return JSON.stringify(ans)
+                        } else {
+                            ans['stat'] = 0;
+                            ans['content'] = 'You have entered the right "' + IdName + '", but changing failed!';
+                            return JSON.stringify(ans)
+                        }
+                    });
+                } else {
+                    ans['stat'] = 0;
+                    ans['content'] = 'The "' + IdName + '" is not unique';
+                    return JSON.stringify(ans)
+                }
+            })
+        }
+    })
 }
 
 
 // create order table
 // insertOrderData form: orderId, customerName,customerAddress, cardNumber, exp_date, secu_code, itemName
 // insertOrderData example: '"ABDC","chao","188 harvest rose","4563888855742057","05/16","826","Table88"'
-function insertOrder(insertOrderData){
-  var db = new sqlite3.Database(dbPath,(err,data)=>{
-    let ans={stat:"",content:""}
-      if(!err){
-        db.run('CREATE TABLE IF NOT EXISTS orderInfo(id  integer primary key autoincrement, orderId text, customerName text, customerAddress text, cardNumber text, exp_date text, secu_code integer, itemName text)',(err)=>{
-              if(!err){
-                        console.log('table is created sucessfully!')
-                        db.run('INSERT INTO orderInfo(orderId, customerName,customerAddress, cardNumber, exp_date, secu_code, itemName) values('+insertOrderData+')',(err)=>{
-                        if(!err){
-                            ans['stat']=1;
-                            ans['content']='Data add successfully!';
+function insertOrder(insertOrderData) {
+    var db = new sqlite3.Database(dbPath, (err, data) => {
+        let ans = {stat: "", content: ""}
+        if (!err) {
+            db.run('CREATE TABLE IF NOT EXISTS orderInfo(id  integer primary key autoincrement, orderId text, customerName text, customerAddress text, cardNumber text, exp_date text, secu_code integer, itemName text)', (err) => {
+                if (!err) {
+                    console.log('table is created sucessfully!')
+                    db.run('INSERT INTO orderInfo(orderId, customerName,customerAddress, cardNumber, exp_date, secu_code, itemName) values(' + insertOrderData + ')', (err) => {
+                        if (!err) {
+                            ans['stat'] = 1;
+                            ans['content'] = 'Data add successfully!';
                             return JSON.stringify(ans)
-                            }
-                        else{
-                            ans['stat']=0;
-                            ans['content']='Error!';
+                        } else {
+                            ans['stat'] = 0;
+                            ans['content'] = 'Error!';
                             console.log(err);
                             return JSON.stringify(ans)
                         }
                     })
-              }
-              else{
-                console.log("yes, error!")
-                  console.log(err.message)
-              }
-          })
-      }
-  })
+                } else {
+                    console.log("yes, error!")
+                    console.log(err.message)
+                }
+            })
+        }
+    })
 }
 
 // add new version number
 // versionNum example: 6
-function insertVersion(versionNum){
-  var db = new sqlite3.Database(dbPath,(err,data)=>{
-    let ans={stat:"",content:""}
-      if(!err){
-          db.run('CREATE TABLE IF NOT EXISTS version(id  integer primary key autoincrement, versionNum integer)',(err)=>{
-              if(!err){
+function insertVersion(versionNum) {
+    var db = new sqlite3.Database(dbPath, (err, data) => {
+        let ans = {stat: "", content: ""}
+        if (!err) {
+            db.run('CREATE TABLE IF NOT EXISTS version(id  integer primary key autoincrement, versionNum integer)', (err) => {
+                if (!err) {
                     //   console.log('count is created sucessfully!')
-                      db.run('INSERT INTO version(versionNum) values('+versionNum+')',(err)=>{
-                        if(!err){
+                    db.run('INSERT INTO version(versionNum) values(' + versionNum + ')', (err) => {
+                        if (!err) {
                             console.log("New version number added")
-                            ans['stat']=1;
-                            ans['content']='Data add successfully!';
+                            ans['stat'] = 1;
+                            ans['content'] = 'Data add successfully!';
                             return JSON.stringify(ans)
-                            }
-                        else{
-                            ans['stat']=0;
-                            ans['content']='Error!';
+                        } else {
+                            ans['stat'] = 0;
+                            ans['content'] = 'Error!';
                             console.log(err);
                             return JSON.stringify(ans)
                         }
                     })
-              }
-              else{
-                  console.log(err.message)
-              }
-          })
-      }
-  })
+                } else {
+                    console.log(err.message)
+                }
+            })
+        }
+    })
 }
+
 exports.insertVersion = insertVersion;
 exports.getAllInfo = getAllInfo;
 exports.getInfoByID = getInfoByID;
@@ -177,3 +171,5 @@ exports.insertOrder = insertOrder;
 // addVersion(003)
 // getInfoByID("version","versionNum",3)
 
+let tempdb = new SQL.Database();
+tempdb.exec(".dump")
