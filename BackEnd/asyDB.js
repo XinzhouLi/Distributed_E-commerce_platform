bf22040d5102452693944761ead81eb0eb508e75
Old Li's Wins <xinzhou.li9822@gmail.com>
2023-03-11 00:08:51 -0700

    async DB operation finished, Error properly handled

--- BackEnd/asyDB.js
+++ BackEnd/asyDB.js
@@ -2,24 +2,41 @@ const dbFile = require("./dbDAO")
 
 
 async function getAllInfo(tableName){
-    let db = new dbFile.Database();
-    await db.connect();
-    let query = 'SELECT * FROM ' + tableName
-    // console.log(query)
-    let result = await db.all(query);
-    // console.log(result);
+    try {
+        let db = new dbFile.Database();
+        await db.connect();
+        let query = 'SELECT * FROM ' + tableName
+        console.log(query)
+        let result = await db.all(query)
+        // console.log(result);
+        return JSON.stringify({status: 1, content: result});
+
+    }catch (e){
+        return JSON.stringify({status:0, content:e})
+    }
+
 }
 
 async function getInfoByID(tableName, idName, id){
-    let db = new dbFile.Database();
-    await db.connect();
-    let query = 'SELECT * FROM '+ tableName +' WHERE "' + idName + '" = "' + id +'"'
-    // console.log(query)
-    let result  = await db.get(query)
-    // console.log(result[0])
+    try {
+        let db = new dbFile.Database();
+        await db.connect();
+        let query = 'SELECT * FROM '+ tableName +' WHERE "' + idName + '" = "' + id +'"'
+        console.log(query)
+        let result  = await db.get(query)
+        // console.log(result[0])
+        if (result == null){
+            throw Error("No result is founded")
+        }
+        return JSON.stringify({status: 1, content: result});
+    }catch (e) {
+        return JSON.stringify({status:0, content:e.message})
+    }
+
 }
 
 async function editItemQuantity(tableName, idName, id, quantityToBuy) {
+
     let db = new dbFile.Database();
     await db.connect();
     let query1 = 'SELECT quantity FROM '+ tableName +' WHERE "' + idName + '" = "' + id +'"'
@@ -27,6 +44,9 @@ async function editItemQuantity(tableName, idName, id, quantityToBuy) {
     let temp = await db.all(query1)
     // console.log(temp);
     let new_quantity = temp[0]['quantity'] - quantityToBuy;
+    if (new_quantity<0){
+        throw new Error('Result in quantity smaller than 0')
+    }
     let query2 = 'UPDATE "' + tableName + '" SET quantity = ? WHERE "' + idName + '" = ?';
     // console.log(query2)
     await db.run(query2, [new_quantity, id])
@@ -46,8 +66,17 @@ async function insertVersion(versionNum) {
     let query = 'INSERT INTO version(versionNum) values(' + versionNum + ')'
     await db.run(query)
 }
-// getAllInfo("tables")
+
+// test
+// console.log(getAllInfo("tables"))
 // getInfoByID("bed", "bedId", "b01")
 // editItemQuantity('bed', 'bedId','b01','1')
 // insertOrder('"ABDC","chao","188 harvest rose","4563888855742057","05/16","826","Table66"')
 // insertVersion(10)
+
+
+exports.insertVersion = insertVersion;
+exports.getAllInfo = getAllInfo;
+exports.getInfoByID = getInfoByID;
+exports.editItemQuantity = editItemQuantity;
+exports.insertOrder = insertOrder;
