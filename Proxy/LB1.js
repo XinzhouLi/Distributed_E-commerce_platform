a43820b572eb24e8f4204d9e7e24d4062e832b6d
Lareina.jo <79848427+LAREINA-JO@users.noreply.github.com>
2023-04-11 00:17:01 -0600

    fix lb1 together

--- Proxy/LB1.js
+++ Proxy/LB1.js
@@ -50,30 +50,35 @@ async function output() {
     socketWithS1.on('connect', function () {
         console.log(totalNumServer);
         totalNumServer++;
-        // record the server number
         thisServer1 = totalNumServer;
         let serverNum1 = {
             socket: socketWithS1,
             serverNum: thisServer1
         };
-        // add server to serverList
         serverList.push(socketWithS1);
-        // record the server number and socket in serverNumList
+        console.log(serverList.length + "--/---/---/connect");
+        // const found = serverNumList.find(element => element === serverNum1);
+        // if(found == undefined) {
+        //     serverNumList.push(serverNum1);
+        // }
+        // else{
+        //     console.log("serverNumList already has this server");
+        //     serverNumList.indexOf(serverNum1).serverNum = thisServer1;
+        //     console.log(serverNumList.indexOf(serverNum1).serverNum+"--/---/---/servernum");
+        // }
         serverNumList.push(serverNum1);
+        console.log(serverNumList.length + "--/---/---/serverNumList");
         console.log('Load Balancer connected to localhost:5101 with Test Server, totalNumServer: ' + totalNumServer);
         console.log('This server is ' + thisServer1 + ' in serverList');
-
-        // when server disconnect, delete it from serverList
         socketWithS1.once("disconnect", (reason) => {
             console.log("5101 disconnected because of " + reason);
-            // server number minus 1
             totalNumServer--;
-            // change the server number of other servers after the deleted server
-            checkThisServer(serverNum1);
-            // delete the server from serverList
+            //console.log("currentServer after dddddddd: " + currentServer);
             serverList.splice(serverNum1.serverNum - 1, 1);
+            checkThisServer(serverNum1);
+            console.log(serverList[0]);
+            console.log(serverList.length + "--/---/---/disconnect");
             console.log("totalNumServer after deleted: " + totalNumServer);
-            // if current server is larger than total server number, change current server to 1
             if (currentServer > totalNumServer) {
                 console.log("totalNumServerNow: " + totalNumServer);
                 console.log("currentServerNow: " + currentServer)
@@ -81,16 +86,17 @@ async function output() {
                 if (currentServer == 0 && totalNumServer != 0) {
                     currentServer = 1;
                 }
-                // if total server number is 0, change current server to 1
                 else if (totalNumServer == 0) {
                     currentServer = 1;
                     totalNumServer = 0;
+                }
+                else if (totalNumServer == 1) {
+                    currentServer = 1;
                 }
                 console.log("if currentServer > total: " + currentServer);
             }
         })
     });
-
 
     // make connection with server 2: port 5200
     var ioWithServer2 = require('socket.io-client');
@@ -100,29 +106,36 @@ async function output() {
 
     socketWithS2.on('connect', function () {
         totalNumServer++;
-        // record the server number
         thisServer2 = totalNumServer;
         let serverNum2 = {
             socket: socketWithS2,
             serverNum: thisServer2
         };
-        // add server to serverList
         serverList.push(socketWithS2);
-        // record the server number and socket in serverNumList
+        console.log(serverList.length + "--/---/---/connect");
+        // const found = serverNumList.find(element => element === serverNum2);
+        // if(found == undefined) {
+        //     serverNumList.push(serverNum2);
+        // }
+        // else{
+        //     console.log("serverNumList already has this server");
+        //     serverNumList.indexOf(serverNum2).serverNum = thisServer2;
+        //     console.log(serverNumList.indexOf(serverNum2).serverNum+"--/---/---/servernum");
+        // }
         serverNumList.push(serverNum2);
+        console.log(serverNumList.length + "--/---/---/serverNumList");
         console.log('Load Balancer connected to localhost:5200 with Test Server, totalNumServer: ' + totalNumServer);
         console.log('This server is ' + thisServer2 + ' in serverList');
-
-        // when server disconnect, delete it from serverList
         socketWithS2.once("disconnect", (reason) => {
             console.log("5200 disconnected because of " + reason);
             totalNumServer--;
-            // change the server number of other servers after the deleted server
-            checkThisServer(serverNum2);
-            // delete the server from serverList
+            //console.log("currentServer after dddddddd: " + currentServer);
             serverList.splice(serverNum2.serverNum - 1, 1);
+            checkThisServer(serverNum2);
+            console.log(serverList[0]);
+
+            console.log(serverList.length + "--/---/---/disconnect");
             console.log("totalNumServer after deleted: " + totalNumServer);
-            // if current server is larger than total server number, change current server to 1
             if (currentServer > totalNumServer) {
                 console.log("totalNumServerNow: " + totalNumServer);
                 console.log("currentServerNow: " + currentServer)
@@ -130,7 +143,6 @@ async function output() {
                 if (currentServer == 0 && totalNumServer != 0) {
                     currentServer = 1;
                 }
-                // if total server number is 0, change current server to 1
                 else if (totalNumServer == 0) {
                     currentServer = 1;
                     totalNumServer = 0;
@@ -149,29 +161,36 @@ async function output() {
     socketWithS3.on('connect', function () {
         console.log(totalNumServer);
         totalNumServer++;
-        // record the server number
         thisServer3 = totalNumServer;
         let serverNum3 = {
             socket: socketWithS3,
             serverNum: thisServer3
         };
-        // add server to serverList
         serverList.push(socketWithS3);
-        // record the server number and socket in serverNumList
+        console.log(serverList.length + "--/---/---/connect");
+        // const found = serverNumList.find(element => element === serverNum3);
+        // if(found == undefined) {
+        //     serverNumList.push(serverNum3);
+        // }
+        // else{
+        //     console.log("serverNumList already has this server");
+        //     serverNumList.indexOf(serverNum3).serverNum = thisServer3;
+        //     console.log(serverNumList.indexOf(serverNum3).serverNum+"--/---/---/servernum");
+        // }
         serverNumList.push(serverNum3);
+        console.log(serverNumList.length + "--/---/---/serverNumList");
         console.log('Load Balancer connected to localhost:5300 with Test Server, totalNumServer: ' + totalNumServer);
         console.log('This server is ' + thisServer3 + ' in serverList');
-
-        // when server disconnect, delete it from serverList
         socketWithS3.once("disconnect", (reason) => {
             console.log("5300 disconnected because of " + reason);
             totalNumServer--;
-            // change the server number of other servers after the deleted server
-            checkThisServer(serverNum3);
-            // delete the server from serverList
+            //console.log("currentServer after dddddddd: " + currentServer);
             serverList.splice(serverNum3.serverNum - 1, 1);
+            checkThisServer(serverNum3);
+            console.log(serverList[0]);
+
+            console.log(serverList.length + "--/---/---/disconnect");
             console.log("totalNumServer after deleted: " + totalNumServer);
-            // if current server is larger than total server number, change current server to 1
             if (currentServer > totalNumServer) {
                 console.log("totalNumServerNow: " + totalNumServer);
                 console.log("currentServerNow: " + currentServer)
@@ -179,7 +198,6 @@ async function output() {
                 if (currentServer == 0 && totalNumServer != 0) {
                     currentServer = 1;
                 }
-                // if total server number is 0, change current server to 1
                 else if (totalNumServer == 0) {
                     currentServer = 1;
                     totalNumServer = 0;
@@ -188,6 +206,12 @@ async function output() {
             }
         })
     });
+
+
+
+
+
+
 
 
 
@@ -277,6 +301,7 @@ async function output() {
 
 
     function checkThisServer(socketObj) {
+        console.log("--------------------   checkThisServer   --------------------")
         let temp = 0;
         serverNumList.forEach(obj => {
             console.log("serverNumList[" + serverNumList.indexOf(obj) + "]: " + obj.serverNum);
@@ -289,9 +314,9 @@ async function output() {
                 temp = serverNumList.indexOf(obj);
             }
         })
+        console.log("Server " + temp + " was deleted")
         serverNumList.splice(temp, 1);
     }
-
     return out;
 }
 
